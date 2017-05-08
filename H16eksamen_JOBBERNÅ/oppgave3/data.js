5a9e86b0d552b31b043ab78728b484d7a2a350f7
olaven <olavsundfoer@gmail.com>
2017-05-08 14:02:19 +0200

    oppgave 3 er påbegynt

---
+++ H16eksamen_JOBBERNÅ/oppgave3/data.js
@@ -0,0 +1,78 @@
+var middagtyper = [];
+middagtyper[1] = "Sørlandsk krabbesuppe";
+middagtyper[2] = "Mandalstorsk i smørsaus med Holumspoteter ";
+middagtyper[3] = "Laks fra Laudal i spinat"//kun dersom middager==3
+
+var ravarer = [
+  barn = {
+    torsk: 200,
+    sei: 200,
+    makrel: 200,
+    reker: 250,
+    krabbe: 300,
+    laks: 200,
+  },
+  ungdom = {
+    torsk: 300,
+    sei: 300,
+    makrel: 300,
+    reker: 500,
+    krabbe: 500,
+    laks: 300,
+  },
+  voksne = {
+    torsk: 350,
+    sei: 350,
+    makrel: 350,
+    reker: 500,
+    krabbe: 600,
+    laks: 350,
+  }
+];
+
+var bestillingstabell = [
+  {
+    uke: 26,
+    antallMiddager: 2,
+    barn: 1,
+    ungdom: 1,
+    voksne: 2,
+  },
+  {
+    uke: 26,
+    antallMiddager: 3,
+    barn: 0,
+    ungdom: 2,
+    voksne: 2,
+  },{
+    uke: 26,
+    antallMiddager: 2,
+    barn: 0,
+    ungdom: 0,
+    voksne: 1,
+  },{
+    uke: 26,
+    antallMiddager: 3,
+    barn: 0,
+    ungdom: 0,
+    voksne: 2,
+  },{
+    uke: 26,
+    antallMiddager: 2,
+    barn: 3,
+    ungdom: 0,
+    voksne: 2,
+  },{
+    uke: 27,
+    antallMiddager: 2,
+    barn: 1,
+    ungdom: 1,
+    voksne: 2,
+  },{
+    uke: 27,
+    antallMiddager: 2,
+    barn: 1,
+    ungdom: 1,
+    voksne: 2,
+  },
+]
